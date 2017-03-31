31fb36815a2dc7af715df9189f5478779608dfef
potatoshane <li2137@purdue.edu>
2017-03-31 14:17:23 -0400

    Fixed imports

--- src/main/java/com/resoose/ResooseApplication.java
+++ src/main/java/com/resoose/ResooseApplication.java
@@ -1,18 +1,10 @@
 package com.resoose;
 
-import com.resoose.service.TokenProvider;
-import javafx.application.Application;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ResooseApplication {
-
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResooseApplication.class, args);
