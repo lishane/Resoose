4723ab4f24999dcebee54ab5bddb5af8646c7a94
potatoshane <li2137@purdue.edu>
2017-03-30 23:36:19 -0400

    Added parameters

--- src/main/java/com/resoose/controller/BusinessController.java
+++ src/main/java/com/resoose/controller/BusinessController.java
@@ -5,6 +5,7 @@ import com.resoose.service.FusionBusiness;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,12 +20,8 @@ public class BusinessController {
     private BusinessFactory businessFactory;
 
     @RequestMapping(path = "/random")
-    public FusionBusiness getRandomBusiness() {
-        return businessFactory.getRandomRestaurant();
+    public FusionBusiness getRandomBusiness(@RequestParam String term, @RequestParam String longitude, @RequestParam String latitude, @RequestParam String price) {
+        return businessFactory.getRandomRestaurant(term, longitude, latitude, price);
     }
 
-    @RequestMapping(path = "/token")
-    public String getToken() {
-        return businessFactory.getFusionToken();
-    }
 }
